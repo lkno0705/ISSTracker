43c6de119a0b82a8f061c406ef11311a7492aacd
lkno0705 <lkno0705@gmail.com>
2020-06-02 00:36:37 +0200

    Add Comments and complete Startup Script for Servers

--- Backend/Core/APIServer.py
+++ Backend/Core/APIServer.py
@@ -3,40 +3,51 @@ from Backend.Requests.issCurrPos import currPos as issCurrentPosition
 from Backend.Requests.rssFeed import rssFeed as rssFeed
 from Backend.Requests.userPosition import getUserPosition as userPosition
 
+# Reimplementign Request Handler with custom Functions to handle GET Requests
 class requestHandler(BaseHTTPRequestHandler):
 
+    # Implements GET request
     def do_GET(self):
+        # Dictonary containg Links assigned with their correct functions
         links = {
-            "ISSpos": issCurrentPosition,
-            "RSS": rssFeed,
-            "userPosition": userPosition
+            "/ISSpos": issCurrentPosition,
+            "/RSS": rssFeed,
+            "/userPosition": userPosition
         }
-        try:
-            data = links.get(self.path)
+
+        # Executing function based on link; Works kinda like a switch case statement
+        function = links.get(self.path)
+        if function is not None:
+            data = function()
             code = 200
-        except:
+        else:
+            # Setting Error Message
             data = '<?xml version="1.0" encoding="UTF-8"?>' \
                    '<message>' \
                    '<error>Error 400: Bad Request</error>' \
                    '</message>'
             code = 400
 
+        # set response code
         self.send_response(code=code)
+
+        # set http header to define body content type as XML
         self.send_header('Content-type', 'text/xml')
         self.end_headers()
+
+        # send body
         self.wfile.write(bytes(str(data), "utf-8"))
 
 
 
 def startAPIServer():
+    # Setting Server address to localhost:8081
     server_address = ('', 8081)
+
+    # Starting http server which handles every request in a seperate THREAD
     httpd = ThreadingHTTPServer(server_address, requestHandler)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-
-
-if __name__ == '__main__':
-    startAPIServer()
--- startServers.py
+++ startServers.py
@@ -1,23 +1,22 @@
 import subprocess
 import multiprocessing
+from Backend.Core.APIServer import startAPIServer
 
 # initialize Thread Array
 threads = []
 
-def startAPI():
-    while True:
-        print("RUNNING")
 
 def startWebserver():
+    # Start Webserver at address localhost:8080 with serving directory ./ISS_Pojekt
     subprocess.call("python3 -m http.server 8080 --bind 127.0.0.1 --directory ./ISS_Projekt/",
                     shell=True)
 
-if __name__ == '__main__':
-    threads.append(multiprocessing.Process(target=startWebserver))
-    threads.append(multiprocessing.Process(target=startAPI))
-    for thread in threads:
-        thread.start()
 
+if __name__ == '__main__':
+    # Append Thread Objects to Thread array
+    threads.append(multiprocessing.Process(target=startAPIServer))
+    threads.append(multiprocessing.Process(target=startWebserver))
     for thread in threads:
-        thread.join()
+        # Start Threads
+        thread.start()
 
