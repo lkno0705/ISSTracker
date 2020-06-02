004a4bb7e9e04ba996a6af97f50d1b1e736eb9b4
Zhibek Abay <zhibekabay@Zhibeks-MacBook-Pro.local>
2020-06-02 14:36:24 +0200

    added comments and return value as XML

--- Backend/Requests/issCurrPos.py
+++ Backend/Requests/issCurrPos.py
@@ -1,8 +1,13 @@
 import requests
 
 def currPos():
+    #Get request to get the current position of ISS from 'http://api.open-notify.org/iss-now.json'
     response = requests.get("http://api.open-notify.org/iss-now.json")
     data = response.json()
+
+    # read data from response
     latitude = data['iss_position']['latitude']
     longitude = data['iss_position']['longitude']
-    return latitude + "," + longitude
+
+    # return values as XML
+    return "<iss_position>" + latitude + "," + longitude + "</iss_position>"
