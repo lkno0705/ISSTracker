8473652164258b26e77090b1a499874ed2c7bfed
Zhibek Abay <zhibekabay@Zhibeks-MacBook-Pro.local>
2020-06-02 11:32:58 +0200

    added current ISS position functionality

--- Backend/Requests/issCurrPos.py
+++ Backend/Requests/issCurrPos.py
@@ -1,3 +1,8 @@
+import requests
 
 def currPos():
-    return 10
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    data = response.json()
+    latitude = data['iss_position']['latitude']
+    longitude = data['iss_position']['longitude']
+    return latitude + "," + longitude
