f1914c7b1009c3cc724cca1616b1a1788c33751e
lkno0705 <lkno0705@gmail.com>
2020-06-05 15:09:51 +0200

    Add first database interaction to save ISSpos data

--- Backend/Core/database.py
+++ Backend/Core/database.py
@@ -1,14 +1,63 @@
 import redis
-
+from time import time
+import datetime
 
 class redisDB:
-    redisHost = "localhost"
-    redisDB = redis.Redis(host=redisHost, port=6379, db=0)
+    __redisHost__ = "localhost"
+    __redisDB__ = redis.Redis(host=__redisHost__, port=6379, db=0)
+
+    def _setIsspos(self, data):
+        # IssPos object (data):
+        # {
+        #   "requestname": "ISSpos",
+        #   "data":{
+        #       "timestamp":"2012-12-15 01:21:05",
+        #       "latitude": "-17.9617",
+        #       "longitude": "162.6117"
+        #   }
+        # }
+        with self.__redisDB__ as DB:
+            DB.set(name="ISSpos:" + data["data"]["timestamp"] + ":latitude", value=data["data"]["latitude"])
+            DB.set(name="ISSpos:" + data["data"]["timestamp"] + ":longitude", value=data["data"]["longitude"])
+            DB.set(name="ISSpos:" + data["data"]["timestamp"] + ":timestamp", value=data["data"]["timestamp"])
+            DB.expire(name="ISSpos:" + data["data"]["timestamp"], time=86400)  # key expires after 24h
 
     def setData(self, data):
-        # parse data to python Object
-        with self.redisDB as DB:
-            for i in range(len(data["data"])):
-                name = data["requestname"] + ":" + data["data"][i]
-                DB.set(name=name, value=data["data"][i])
-                DB.expire(name=name, time=86400)
+        # TODO: parse data to python Object
+        topLevel = {
+            "ISSpos": self._setIsspos,
+            "RSS-Feed": "_setRSS"
+        }
+        topLevel.get(data["requestname"])(data)
+
+    def getData(self, data, n=1):
+        # data has to be an correctly formatted XML with an empty data Tag:
+        # <?xml version="1.0" encoding="UTF-8"?>
+        # <Request>
+        #    <requestName>ISS-Pos</requestName>
+        #    <data>
+        #    </data>
+        # </Request>
+        #
+        # TODO: parse data to python Object
+        with self.__redisDB__ as DB:
+            keys = DB.keys(pattern="ISSpos:*")
+        print(keys)
+
+
+if __name__ == '__main__':
+    data = {
+        "requestname": "ISSpos",
+        "data": {
+            "latitude": 1234,
+            "longitude": 1234,
+            "timestamp": datetime.datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H-%M-%S')
+        }
+    }
+    DB = redisDB()
+    DB.setData(data=data)
+
+    datar = {
+        "requestname": "ISSpos",
+    }
+    DB.getData(datar, n=2)
