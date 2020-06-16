eccc37c37057867d1de4f444bb35cfdbe7f70593
lawand.allo <lawand.allo@sap.com>
2020-06-17 01:18:24 +0200

    converted time to utc

--- Backend/Tools/rssFeedCalls.py
+++ Backend/Tools/rssFeedCalls.py
@@ -1,8 +1,8 @@
 import redis
-from dateutil import parser as dateParser
 
 from Backend.Requests import rssFeed
 from Backend.Requests import stationReportFeed
+from Backend.Tools import rssFeedTimeConverter as timeConverter
 
 
 def setRssFeed(data):
@@ -11,20 +11,16 @@ def setRssFeed(data):
     feedName = data['rssFeedName']
     feedItems = data['items']
     for i in range(len(feedItems)):
-        timestamp = feedItems[i]['published']
-        # get timezone on position 23 from string of timestamp
-        timezone = timestamp[23:]
-        # parse timestamp to format: 2014-07-17 17:08:00 EDT
-        timestamp = dateParser.parse(timestamp, ignoretz=True)
-        timestamp = str(timestamp) + " " + str(timezone)
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + str(timestamp) + ":title:", value=feedItems[i]['title'])
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + str(timestamp) + ":summary:", value=feedItems[i]['summary'])
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + str(timestamp) + ":published:", value=feedItems[i]['published'])
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + str(timestamp) + ":link:", value=feedItems[i]['link'])
-        print("RSS-Feed:" + feedName + ":" + str(timestamp) + ":title:" + str(feedItems[i]['title']))
-        print("RSS-Feed:" + feedName + ":" + str(timestamp) + ":summary:" + str(feedItems[i]['summary']))
-        print("RSS-Feed:" + feedName + ":" + str(timestamp) + ":published:" + str(feedItems[i]['published']))
-        print("RSS-Feed:" + feedName + ":" + str(timestamp) + ":link:" + str(feedItems[i]['link']))
+        time = feedItems[i]['published']
+        time = timeConverter.convert(time)
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":title:", value=feedItems[i]['title'])
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":summary:", value=feedItems[i]['summary'])
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":published:", value=feedItems[i]['published'])
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":link:", value=feedItems[i]['link'])
+        print("RSS-Feed:" + feedName + ":" + str(time) + ":title:" + str(feedItems[i]['title']))
+        print("RSS-Feed:" + feedName + ":" + str(time) + ":summary:" + str(feedItems[i]['summary']))
+        print("RSS-Feed:" + feedName + ":" + str(time) + ":published:" + str(feedItems[i]['published']))
+        print("RSS-Feed:" + feedName + ":" + str(time) + ":link:" + str(feedItems[i]['link']))
 
 
 # set spacetoground rssFeed
