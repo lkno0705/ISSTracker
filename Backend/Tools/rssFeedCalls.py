96d58a9a867de10af33817a7db6946f4ce1cf7fa
lawand.allo <lawand.allo@sap.com>
2020-06-17 11:07:12 +0200

    return sorted rssFeeds

--- Backend/Tools/rssFeedCalls.py
+++ Backend/Tools/rssFeedCalls.py
@@ -18,18 +18,18 @@ def setRssFeed(data):
         __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":summary", value=feedItems[i]['summary'], ex=3600)
         __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":published", value=time, ex=3600)
         __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":link", value=feedItems[i]['link'], ex=3600)
-        print("RSS-Feed:" + feedName + ":" + time + ":title:" + str(feedItems[i]['title']))
-        print("RSS-Feed:" + feedName + ":" + time + ":summary:" + str(feedItems[i]['summary']))
-        print("RSS-Feed:" + feedName + ":" + time + ":published:" + time)
-        print("RSS-Feed:" + feedName + ":" + time + ":link:" + str(feedItems[i]['link']))
+        # print("RSS-Feed:" + feedName + ":" + time + ":title:" + str(feedItems[i]['title']))
+        # print("RSS-Feed:" + feedName + ":" + time + ":summary:" + str(feedItems[i]['summary']))
+        # print("RSS-Feed:" + feedName + ":" + time + ":published:" + time)
+        # print("RSS-Feed:" + feedName + ":" + time + ":link:" + str(feedItems[i]['link']))
 
 
-# # set spacetoground rssFeed
-# data = rssFeed.rssFeed()
-# setRssFeed(data)
-# # set stationreport rssFeed
-# data = stationReportFeed.stationReportFeed()
-# setRssFeed(data)
+# set spacetoground rssFeed
+data = rssFeed.rssFeed()
+setRssFeed(data)
+# set stationreport rssFeed
+data = stationReportFeed.stationReportFeed()
+setRssFeed(data)
 
 
 # awaits utc time in format yyyy-mm-dd HH-MM-SS!!
@@ -46,7 +46,8 @@ def getRssFeed(time, numbOfItems):
         if len(items) < numbOfItems and str(__redisDB__.get(key + ':published')) <= time:
             items.append({'title': __redisDB__.get(key + ':title'), 'summary': __redisDB__.get(key + ':summary'),
                           'published': __redisDB__.get(key + ':published'), 'link': __redisDB__.get(key + ':link')})
-    return items
+
+    return sorted(items, key=lambda i: (i['published']),reverse=True)
 
 
-# print(*getRssFeed('2020-07-05 16-36-00', 10), sep='\n')
+print(*getRssFeed('2018-07-05 16-36-00', 40), sep='\n')
