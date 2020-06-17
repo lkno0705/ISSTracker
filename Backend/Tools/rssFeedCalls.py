216c1ef9724a7c45e2a4edd983048ca83654de4d
lawand.allo <lawand.allo@sap.com>
2020-06-17 14:25:16 +0200

    small refactorings

--- Backend/Tools/rssFeedCalls.py
+++ Backend/Tools/rssFeedCalls.py
@@ -2,7 +2,7 @@ import redis
 
 from Backend.Requests import rssFeed
 from Backend.Requests import stationReportFeed
-from Backend.Tools import rssFeedTimeConverter as timeConverter
+from Backend.Tools import rssFeedTimeConverter as dateConverter
 
 __redisHost__ = "localhost"
 __redisDB__ = redis.StrictRedis(host=__redisHost__, port=6379, db=0, decode_responses=True)
@@ -12,24 +12,25 @@ def setRssFeed(data):
     feedName = data['rssFeedName']
     feedItems = data['items']
     for i in range(len(feedItems)):
-        time = feedItems[i]['published']
-        time = timeConverter.convert(time)
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":title", value=feedItems[i]['title'], ex=3600)
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":summary", value=feedItems[i]['summary'], ex=3600)
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":published", value=time, ex=3600)
-        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + time + ":link", value=feedItems[i]['link'], ex=3600)
-        # print("RSS-Feed:" + feedName + ":" + time + ":title:" + str(feedItems[i]['title']))
-        # print("RSS-Feed:" + feedName + ":" + time + ":summary:" + str(feedItems[i]['summary']))
-        # print("RSS-Feed:" + feedName + ":" + time + ":published:" + time)
-        # print("RSS-Feed:" + feedName + ":" + time + ":link:" + str(feedItems[i]['link']))
+        publishDate = feedItems[i]['published']
+        publishDate = dateConverter.convert(publishDate)
+        expireTime = 3600  # in seconds
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + publishDate + ":title", value=feedItems[i]['title'], ex=expireTime)
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + publishDate + ":summary", value=feedItems[i]['summary'], ex=expireTime)
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + publishDate + ":published", value=publishDate, ex=expireTime)
+        __redisDB__.set(name="RSS-Feed:" + feedName + ":" + publishDate + ":link", value=feedItems[i]['link'], ex=expireTime)
+        print("RSS-Feed:" + feedName + ":" + publishDate + ":title:" + str(feedItems[i]['title']))
+        print("RSS-Feed:" + feedName + ":" + publishDate + ":summary:" + str(feedItems[i]['summary']))
+        print("RSS-Feed:" + feedName + ":" + publishDate + ":published:" + publishDate)
+        print("RSS-Feed:" + feedName + ":" + publishDate + ":link:" + str(feedItems[i]['link']))
 
 
-# set spacetoground rssFeed
-data = rssFeed.rssFeed()
-setRssFeed(data)
-# set stationreport rssFeed
-data = stationReportFeed.stationReportFeed()
-setRssFeed(data)
+# # set spacetoground rssFeed
+# data = rssFeed.rssFeed()
+# setRssFeed(data)
+# # set stationreport rssFeed
+# data = stationReportFeed.stationReportFeed()
+# setRssFeed(data)
 
 
 # awaits utc time in format yyyy-mm-dd HH-MM-SS!!
@@ -47,7 +48,7 @@ def getRssFeed(time, numbOfItems):
             items.append({'title': __redisDB__.get(key + ':title'), 'summary': __redisDB__.get(key + ':summary'),
                           'published': __redisDB__.get(key + ':published'), 'link': __redisDB__.get(key + ':link')})
 
-    return sorted(items, key=lambda i: (i['published']),reverse=True)
+    return sorted(items, key=lambda i: (i['published']), reverse=True)
 
 
-print(*getRssFeed('2018-07-05 16-36-00', 40), sep='\n')
+# print(*getRssFeed('2018-07-05 16-36-00', 40), sep='\n')
