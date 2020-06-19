f27639db07fb75181b86212507c8ecc730bb09f9
BigKahuna7385 <danielburger2@gmx.de>
2020-06-19 15:44:13 +0200

    will change the css animation to a movement to the left or right instead of a animation in width. The left sidebar will no longer be infront of the map an block the cursor while invsisible.

--- Frontend/javascript/sidebar.js
+++ Frontend/javascript/sidebar.js
@@ -2,35 +2,35 @@
 
 // left menu
 function toggleNavL() {   
-  var x = document.getElementById("mySidebarLeft").style.width;
-    if (document.getElementById("mySidebarLeft").style.width == "250px" || document.getElementById("mySidebarLeft").style.width == "")
+  var x = document.getElementById("mySidebarLeft").style.left;
+    if (document.getElementById("mySidebarLeft").style.left == "0px" || document.getElementById("mySidebarLeft").style.left == "")
     {
-      document.getElementById("mySidebarLeft").style.width = "0";
+      document.getElementById("mySidebarLeft").style.left = "-250px";
       document.getElementById("mainLeft").style.marginLeft = "-250px";
       document.getElementById("arrowleft").style.transform = "rotate(180deg)";
     }
     else
     {
-      document.getElementById("mySidebarLeft").style.width = "250px";
-      document.getElementById("mainLeft").style.marginLeft = "0";
+      document.getElementById("mySidebarLeft").style.left = "0px";
+      document.getElementById("mainLeft").style.marginLeft = "0px";
       document.getElementById("arrowleft").style.transform = "rotate(0deg)";
     }
 }  
 
 //  right menu
   function toggleNav() {   
-    var x = document.getElementById("mySidebar").style.width;
-      if (document.getElementById("mySidebar").style.width == "250px")
+    var x = document.getElementById("mySidebar").style.right;
+      if (document.getElementById("mySidebar").style.right == "-250px" || document.getElementById("mySidebar").style.right == "" )
       {
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("main").style.marginRight = "0";
-        document.getElementById("arrowright").style.transform = "rotate(0deg)";
+        document.getElementById("mySidebar").style.right = "0";
+        document.getElementById("main").style.marginRight = "250px";
+        document.getElementById("arrowright").style.transform = "rotate(180deg)";
       }
       else
       {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginRight = "250px";
-        document.getElementById("arrowright").style.transform = "rotate(180deg)";
+        document.getElementById("mySidebar").style.right = "-250px";
+        document.getElementById("main").style.marginRight = "0px";
+        document.getElementById("arrowright").style.transform = "rotate(0deg)";
       }
   }     
 
@@ -39,4 +39,5 @@ function toggleNavL() {
     document.getElementById("mySidebarLeft").style.backgroundColor = "#111";
     document.getElementById("openbtnLeft").style.display = "";
     document.getElementById("sliderLeft").style.display = "";
+    document.getElementById("sliderLeft").style.pointerEvents = "auto";
   }
