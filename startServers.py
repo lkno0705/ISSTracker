b78b03984c19870591e1d7cb3cc9cc3b52c9e0a4
lkno0705 <lkno0705@gmail.com>
2020-06-19 19:38:36 +0200

    Add RSS-Feed polling

--- startServers.py
+++ startServers.py
@@ -1,6 +1,7 @@
 import subprocess
 import multiprocessing
 from Backend.Core.APIServer import startAPIServer
+from Backend.Core.polling import polling
 
 # initialize Thread Array
 threads = []
@@ -16,6 +17,7 @@ if __name__ == '__main__':
     # Append Thread Objects to Thread array
     threads.append(multiprocessing.Process(target=startAPIServer))
     threads.append(multiprocessing.Process(target=startWebserver))
+    threads.append(multiprocessing.Process(target=polling))
     for thread in threads:
         # Start Threads
         thread.start()
