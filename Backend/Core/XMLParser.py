582078f559931e94b8b60a5d50aadad126e79d09
lawand.allo <lawand.allo@sap.com>
2020-06-20 09:43:43 +0200

    moved astrosXMLParser to main XMLParser

--- Backend/Core/XMLParser.py
+++ Backend/Core/XMLParser.py
@@ -72,16 +72,60 @@ def convertISSDBKeyToXML(requestData):
     elem.append(dataChild)
     return elem
 
+
+# XML-Structure for AstrosOnISS
+# <Request>
+#   <requestName>AstrosOnISS</requestName>
+#   <data>
+#       <Astro name="max muster">
+#          <picture>link</picture>
+#          <flag>link</flag>
+#          <nation>link</nation>
+#       </Astro>
+#       <Astro name="max muster">
+#          <picture>link</picture>
+#          <flag>link</flag>
+#          <nation>link</nation>
+#       </Astro>
+#   </data>
+# </Request>'
+
+def convertAstrosToXML(requestData):
+    elem = Element('Request')
+    requestChild = Element("requestName")
+    requestChild.text = "AstrosOnISS"
+    elem.append(requestChild)
+    dataChild = Element("data")
+    for astro in requestData:
+        AstroChild = Element("Astro")
+        AstroChild.attrib = {"name": astro.name}
+        picture = Element("picture")
+        flag = Element("flag")
+        nation = Element("nation")
+        picture.text = astro.pic
+        flag.text = astro.flag
+        nation.text = astro.nation
+        AstroChild.append(picture)
+        AstroChild.append(flag)
+        AstroChild.append(nation)
+        dataChild.append(AstroChild)
+    elem.append(dataChild)
+    return tostring(elem)
+
+
 def convertISSPosToXML(requestData):
     return 10
+
 
 def reformatData(requestData, requestName):
     functions = {
         'ISSpos': convertISSPosToXML,
-        'ISSDB': convertISSDBKeyToXML
+        'ISSDB': convertISSDBKeyToXML,
+        'AstrosOnISS': convertAstrosToXML
         # List of Requests
     }
 
     return functions.get(requestName)(requestData)
 
 # print(tostring(reformatData(l, "ISSDB")))
+# print(convertAstrosToXML(database.redisDB._getAstros(database.redisDB, None)))
