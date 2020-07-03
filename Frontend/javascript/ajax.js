9793ab805ba9a786beb24d3ee0ec0cf01d5890ee
BigKahuna7385 <danielburger2@gmx.de>
2020-07-03 21:21:04 +0200

    dtd in ajax call

--- Frontend/javascript/ajax.js
+++ Frontend/javascript/ajax.js
@@ -2,7 +2,7 @@ var ajaxCall = function(oData){
     if(!oData.e)
         oData.e= "";
     if (oData.type == "POST")
-     oData.data = "<?xml version='1.0' encoding='UTF-8'?>" + oData.data;
+     oData.data = "<?xml version='1.0' encoding='UTF-8'?>" + '<!DOCTYPE Request SYSTEM "./DTD/' + oData.call +'.dtd">' + oData.data;
     $.ajax({
         url: 'http://127.0.0.1:8082/' + oData.call,
         data: oData.data,
